d27ffd891c317709fca2b0cc3a46ecf48fa54a76
collinsbett29 <collinsbett29@gmail.com>
2019-10-27 16:59:05 +0300

    Test the unit tests

---
+++ manage.py
@@ -0,0 +1,21 @@
+from app import create_app
+from flask_script import Manager,Shell,Server
+
+# creating app instance
+app = create_app('development')
+manager = Manager(app)
+
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(testsg)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
