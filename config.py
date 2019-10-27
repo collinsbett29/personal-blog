ac0fb461bf0a4afaf3c011f66381d43b85ba1661
collinsbett29 <collinsbett29@gmail.com>
2019-10-27 16:57:34 +0300

    Disable SQLAlchemy modification

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config:    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
