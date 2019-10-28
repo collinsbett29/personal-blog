678cfe483fb8f3883152f71631a0a2aff737a5d4
collinsbett29 <collinsbett29@gmail.com>
2019-10-28 16:38:06 +0300

    Add a secret key

--- config.py
+++ config.py
@@ -5,15 +5,30 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     @staticmethod
     def init_app(app):
         pass
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")   
+    
+    DEBUG = True
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:password@localhost/blogs'
     DEBUG = True
 
 config_options = {
