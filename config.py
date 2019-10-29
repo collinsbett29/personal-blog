a504e67b7b3dfc82ad90f675954c8acc8ef2a232
collinsbett29 <collinsbett29@gmail.com>
2019-10-29 15:50:01 +0300

    Create a new configuration that specifies destination

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:    
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
