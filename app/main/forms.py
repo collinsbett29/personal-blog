d6ddce9b6e67b7ebd7e4f288beb8626d1168ec39
collinsbett29 <collinsbett29@gmail.com>
2019-10-29 12:03:08 +0300

    Add a bio section

--- app/main/forms.py
+++ app/main/forms.py
@@ -17,3 +17,7 @@ class CommentForm(FlaskForm):
 class BlogForm(FlaskForm):
     name = TextAreaField('Blog')
     submit = SubmitField()
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
