7a9330a8f405eaad824424a50681405849668223
collinsbett29 <collinsbett29@gmail.com>
2019-10-30 11:04:01 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:    
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # simple mde  configurations
@@ -28,7 +28,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:##@@collins1@localhost/blogs'
     pass
 
 config_options = {
